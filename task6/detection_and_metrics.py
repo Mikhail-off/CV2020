a7b3636552d258062e5ea932fd066acb69d90593
Nikita Mikhaylov <nikita.mikhaylov@abbyy.com>
2020-02-26 20:35:51 +0300

    hw6

---
+++ task6/detection_and_metrics.py
@@ -0,0 +1,96 @@
+# ============================== 1 Classifier model ============================
+
+def get_cls_model(input_shape):
+    """
+    :param input_shape: tuple (n_rows, n_cols, n_channgels)
+            input shape of image for classification
+    :return: nn model for classification
+    """
+    # your code here \/
+    from tensorflow.keras.models import Sequential
+    return Sequential()
+    # your code here /\
+
+def fit_cls_model(X, y):
+    """
+    :param X: 4-dim ndarray with training images
+    :param y: 2-dim ndarray with one-hot labels for training
+    :return: trained nn model
+    """
+    # your code here \/
+    model = get_cls_model((40, 100, 1))
+    # model.fit(...)
+    return model
+    # your code here /\
+
+
+# ============================ 2 Classifier -> FCN =============================
+def get_detection_model(cls_model):
+    """
+    :param cls_model: trained cls model
+    :return: fully convolutional nn model with weights initialized from cls
+             model
+    """
+    # your code here \/
+    # detection_model = ...
+    # return detection_model
+    # your code here /\
+
+
+# ============================ 3 Simple detector ===============================
+def get_detections(detection_model, dictionary_of_images):
+    """
+    :param detection_model: trained fully convolutional detector model
+    :param dictionary_of_images: dictionary of images in format
+        {filename: ndarray}
+    :return: detections in format {filename: detections}. detections is a N x 5
+        array, where N is number of detections. Each detection is described
+        using 5 numbers: [row, col, n_rows, n_cols, confidence].
+    """
+    # your code here \/
+    return {}
+    # your code here /\
+
+
+# =============================== 5 IoU ========================================
+def calc_iou(first_bbox, second_bbox):
+    """
+    :param first bbox: bbox in format (row, col, n_rows, n_cols)
+    :param second_bbox: bbox in format (row, col, n_rows, n_cols)
+    :return: iou measure for two given bboxes
+    """
+    # your code here \/
+    return 1
+    # your code here /\
+
+
+# =============================== 6 AUC ========================================
+def calc_auc(pred_bboxes, gt_bboxes):
+    """
+    :param pred_bboxes: dict of bboxes in format {filename: detections}
+        detections is a N x 5 array, where N is number of detections. Each
+        detection is described using 5 numbers: [row, col, n_rows, n_cols,
+        confidence].
+    :param gt_bboxes: dict of bboxes in format {filenames: bboxes}. bboxes is a
+        list of tuples in format (row, col, n_rows, n_cols)
+    :return: auc measure for given detections and gt
+    """
+    # your code here \/
+    return 1
+    # your code here /\
+
+
+# =============================== 7 NMS ========================================
+def nms(detections_dictionary, iou_thr):
+    """
+    :param detections_dictionary: dict of bboxes in format {filename: detections}
+        detections is a N x 5 array, where N is number of detections. Each
+        detection is described using 5 numbers: [row, col, n_rows, n_cols,
+        confidence].
+    :param iou_thr: IoU threshold for nearby detections
+    :return: dict in same format as detections_dictionary where close detections
+        are deleted
+    """
+    # your code here \/
+    return {}
+    # your code here /\
