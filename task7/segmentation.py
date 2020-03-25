8f01572368fe9af16bb4b762398b6fb8295cc8ea
Nikita Mikhaylov <nikita.mikhaylov@abbyy.com>
2020-03-25 19:38:02 +0300

    8

--- task7/segmentation.py
+++ task7/segmentation.py
@@ -1,14 +1,18 @@
 import os
 import numpy as np
-from skimage.io import imread
+from skimage.io import imread, imshow
 from skimage.transform import resize
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-from tensorflow.keras.models import *
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import *
-from tensorflow.keras.optimizers import Adam
 
-BATCH_SIZE = 4
+import matplotlib.pyplot as plt
+
+import keras
+from keras.layers import Concatenate, Conv2D, UpSampling2D, concatenate
+from keras.models import Model, load_model
+from keras.applications.mobilenet_v2 import MobileNetV2
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+
+BATCH_SIZE = 16
 IMAGE_SIZE = 224
 
 def normalize_image(img_as_array):
@@ -17,30 +21,34 @@ def normalize_image(img_as_array):
 def predict(model, img_path):
     image = imread(img_path)
     h, w = image.shape[:2]
+
     image = normalize_image(image)
     image = resize(image, (IMAGE_SIZE, IMAGE_SIZE, 3))
+
     image_with_batch_dim = np.array([image])
     pred = model.predict(image_with_batch_dim)[0]
     if len(pred.shape) == 3:
         pred = np.reshape(pred, pred.shape[:2])
     if h is not None and w is not None:
          pred = resize(pred, (h, w))
-    return pred
+    #plt.imshow(pred)
+    #plt.waitforbuttonpress()
+    return np.clip(pred, 0, 1).astype(np.float32)
 
 def data_generators(train_data_dir, gt_data_dir):
     data_generator = ImageDataGenerator(preprocessing_function=normalize_image).flow_from_directory(train_data_dir, batch_size=BATCH_SIZE,
                                                               class_mode=None, target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                                              color_mode="rgb")
+                                                              color_mode="rgb", seed=42)
     gt_generator = ImageDataGenerator(rescale=1./255.).flow_from_directory(gt_data_dir, batch_size=BATCH_SIZE,
                                                                          class_mode=None, target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                                                         color_mode="grayscale")
+                                                                         color_mode="grayscale", seed=42)
     train_data_generator = zip(data_generator, gt_generator)
     train_data_generator = (pair for pair in train_data_generator)
     return train_data_generator
 
 
 def build_model():
-    encoder = MobileNetV2((224, 224, 3), alpha=0.5, include_top=False)
+    encoder = MobileNetV2((224, 224, 3), alpha=1.0, include_top=False)
 
     encoder_output = encoder.output
 
@@ -76,12 +84,12 @@ def build_model():
         UpSampling2D(size=(2, 2))(conv9))
     conv10 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(up10)
     conv10 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv10)
-    conv10 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv10)
-    conv11 = Conv2D(1, 1, activation='sigmoid')(conv10)
+    #conv10 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv10)
+    conv11 = Conv2D(1, 3, activation='sigmoid', padding='same')(conv10)
 
     model = Model(encoder.input, conv11)
 
-    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=3e-4), loss='binary_crossentropy', metrics=['mse'])
 
     model.summary()
 
@@ -92,10 +100,20 @@ def train_model(train_data_path):
     images_train = os.path.join(train_data_path, 'images')
     gt = os.path.join(train_data_path, 'gt')
     train_data_generator = data_generators(images_train, gt)
-    model = build_model()
-    model.fit_generator(train_data_generator, steps_per_epoch=524, epochs=7)
+
+    #x, y = next(train_data_generator)
+    #x, y = x[0], y[0][:, :, 0]
+    #print(x)
+    #plt.imshow((x * 127.5 + 127.5).astype(np.uint8))
+    #plt.waitforbuttonpress()
+
+
+    #model = build_model()
+    model = load_model('segmentation_model.hdf5')
+    model.fit_generator(train_data_generator, steps_per_epoch=524, epochs=1)
+    model.save('segmentation_model.hdf5')
     return model
-    #model.save('segmentation_model.hdf5')
+
 
 if __name__ == '__main__':
-    train_model('C:\\CV2020\\task7\\tests\\00_test_val_input\\train')
+    train_model('D:\\CV2020\\task7\\tests\\00_test_val_input\\train')
